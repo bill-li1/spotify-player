d48ff449dd209cb65219bf05c0791abf5bdb9621
Bill Li <billli@gmail.com>
2023-01-10 18:05:23 -0500

    add next eslint

--- next.config.js
+++ next.config.js
@@ -4,19 +4,20 @@
 /* eslint-env node */
 module.exports = {
   reactStrictMode: true,
+  extends: ['plugin:@next/next/recommended'],
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "i.scdn.co",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "mosaic.scdn.co",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'mosaic.scdn.co',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
