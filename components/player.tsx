15de4e9889923ffd14f6a9ead805ca5931075bdf
Bill Li <billli@gmail.com>
2024-01-09 15:43:23 -0500

    working state with no web player

--- components/player.tsx
+++ components/player.tsx
@@ -1,5 +1,5 @@
 import { animated, to, useSprings } from "react-spring";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 import Image from "next/image";
 import Navbar from "./navbar";
@@ -44,13 +44,13 @@ interface CardProps {
 
 const Player = ({ playlistData }: CardProps) => {
   const spotifyApi = useSpotify();
-  const { data: session } = useSession();
   const [gone] = useState(() => new Set());
   const [animationFinished, setAnimationFinished] = useState<number[]>([]);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [canStartPlaying, setCanStartPlaying] = useState(false);
   const [currentSong, setCurrentSong] = useState(
     playlistData[playlistData.length - 1],
   );
+
   const [cards, set] = useSprings(playlistData.length, (i) => ({
     ...after(i),
     from: before(),
@@ -58,10 +58,6 @@ const Player = ({ playlistData }: CardProps) => {
       setAnimationFinished((prev) => [...prev, i]);
     },
   }));
-
-  const [is_paused, setPaused] = useState<boolean>(false);
-  const [isActive, setIsActive] = useState<boolean>(false);
-  let player = useRef(null);
 
   const bind = useDrag(
     ({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
@@ -97,69 +93,43 @@ const Player = ({ playlistData }: CardProps) => {
   );
 
   useEffect(() => {
-    if (isPlaying && isActive) {
-      spotifyApi.play({
-        uris: [currentSong.uri],
-      });
-    }
-  }, [isPlaying, currentSong]);
-
-  useEffect(() => {
     if (animationFinished.length >= playlistData.length - 6) {
-      setIsPlaying(true);
+      setCanStartPlaying(true);
     }
   }, [animationFinished]);
 
   useEffect(() => {
-    (async () => {
-      const script = document.createElement("script");
-      script.src = "https://sdk.scdn.co/spotify-player.js";
-      script.async = true;
+    if (canStartPlaying) {
+      spotifyApi.play({
+        uris: [currentSong.uri],
+      });
+    }
+  }, [currentSong, canStartPlaying]);
 
-      document.body.appendChild(script);
-
-      window.onSpotifyWebPlaybackSDKReady = async () => {
-        const player = new window.Spotify.Player({
-          name: "GOAT",
-          getOAuthToken: (cb) => {
-            cb(session?.user?.accessToken as string);
-          },
-          volume: 0.5,
-        });
-
-        player.addListener("ready", ({ device_id }) => {
-          console.log("Ready with Device ID", device_id);
-        });
-
-        player.addListener("not_ready", ({ device_id }) => {
-          console.log("Device ID has gone offline", device_id);
-        });
-
-        player.addListener("player_state_changed", async (state) => {
-          if (!state || !player) {
-            console.log("no state");
-            return;
-          }
-
-          setPaused(state.paused);
-          console.log("state", state);
-
-          console.log("player", player);
-          const newState = await player.getCurrentState();
-          console.log("newState", newState);
-        });
-
-        player.connect();
-      };
-    })();
-  }, [session?.user?.accessToken]);
-
-  if (!player) {
-    return <div>Loading...</div>;
-  }
-
-  if (!isActive) {
-    return <div>Not Active</div>;
+  if (!spotifyApi) {
+    return (
+      <div
+        role="status"
+        className="flex min-h-screen flex-col items-center justify-center"
+      >
+        <svg
+          aria-hidden="true"
+          className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-[#1DB954]"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+      </div>
+    );
   }
 
   return (
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,6 @@
 import { ClientSafeProvider, getProviders, signIn } from "next-auth/react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import styles from "../components/button.module.css";
 
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
@@ -11,40 +12,51 @@ const Login: NextPage = ({
   const { from } = router.query;
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      {Object.values(providers).map((provider) => (
-        <div key={(provider as ClientSafeProvider).name}>
-          <button
-            type="button"
-            onClick={() =>
-              signIn((provider as ClientSafeProvider).id, {
-                callbackUrl: from ? `/${from}` : "/",
-              })
-            }
-            className="text-white bg-[#1DB954] hover:bg-[#1BA84C]/90 focus:ring-4 focus:outline-none focus:ring-[#189544]/50 font-medium rounded-lg text-xl px-4 py-2 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-brand-spotify mr-1"
-              width="28"
-              height="38"
-              viewBox="0 0 24 24"
-              strokeWidth="2.2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              {" "}
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />{" "}
-              <circle cx="12" cy="12" r="9" />{" "}
-              <path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" />{" "}
-              <path d="M9 15c1.5 -1 4 -1 5 .5" />{" "}
-              <path d="M7 9c2 -1 6 -2 10 .5" />{" "}
-            </svg>
-            {(provider as ClientSafeProvider).name}
-          </button>
+      <div className="mb-24 flex flex-col items-center justify-center">
+        <h1 className="text-8xl text-center font-bold">
+          The Prettiest <br />
+          <span className="text-[#1DB954]">Spotify</span> Web Player
+        </h1>
+        <div className="mt-8">
+          {Object.values(providers).map((provider) => (
+            <div key={(provider as ClientSafeProvider).name}>
+              <button
+                type="button"
+                onClick={() =>
+                  signIn((provider as ClientSafeProvider).id, {
+                    callbackUrl: from ? `/${from}` : "/",
+                  })
+                }
+                className={styles.btn}
+                // className="text-white bg-[#1DB954] hover:bg-[#1BA84C]/90 focus:ring-4 focus:outline-none focus:ring-[#189544]/50 font-medium rounded-3xl text-2xl px-6 py-2 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+              >
+                <div className={styles.btnContent}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-brand-spotify mr-2"
+                    width="28"
+                    height="38"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2.4"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    {" "}
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />{" "}
+                    <circle cx="12" cy="12" r="10" />{" "}
+                    <path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" />{" "}
+                    <path d="M9 15c1.5 -1 4 -1 5 .5" />{" "}
+                    <path d="M7 9c2 -1 6 -2 10 .5" />{" "}
+                  </svg>
+                  Log In
+                </div>
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
